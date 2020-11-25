e35054e41c70cbe7bb120cac9421095e307f0737
Suhaib Mohammed <suhaib@ebi.ac.uk>
2020-11-25 13:00:42 +0000

    null values if exist skip through jq ? and multiple value separator for a field implemented

--- bin/annotations_from_ensembl.sh
+++ bin/annotations_from_ensembl.sh
@@ -2,92 +2,109 @@
 
 scriptDir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-
 IFS="
 "
-
 config=$1
 species=$(basename $config |  sed 's/\.[^ ]*//g')
 
+## check env variables
 [ ! -z ${ENSEMBL_JSON_PATH+x} ] || (echo "Env var ENSEMBL_JSON_PATH not defined." && exit 1)
 
-## load property fields
+## export property fields
 while read property_field; do 
-    export $property_field; 
+  export $property_field; 
 done < $config
 
 if [ ! -s "$ENSEMBL_JSON_PATH/$species/${species}_genes.json" ]; then
    echo "ERROR : $ENSEMBL_JSON_PATH/$species/${species}_genes.json doesn't exist"
 fi
 
+
+## print multiple values for a field with separator '@@'
+mutiple_values_with_separator () {
+  json_row=$1
+  property_field=$2
+
+  number_of_values=$(echo "$json_row" | jq -r "$property_field" | wc -l)
+
+  if [ $number_of_values -gt 1 ]; then
+    echo "$json_row" | jq -r "$property_field" | tr '\n' '@@'
+  else
+    echo "$json_row" | jq -r "$property_field"
+  fi  
+}
+
 # header file
 cat $config | grep 'property' | awk -F"=" '{ print $1 }' | sed 's/property_//g' | tr '\n' '\t' > $species.ensgene.tsv
 
 for k in $(cat "$ENSEMBL_JSON_PATH/$species/${species}_genes.json" | jq -cn --stream 'fromstream(1|truncate_stream(inputs))' | head -n2); do 
  
-    ensgene=$(echo "$k" | jq "$property_ensgene")
-    echo "ensgene - $ensgene"
-    
-    #mirbase_accession=$(echo "$k" | jq  "$property_mirbase_accession")
-    #echo "mirbase_accession - $mirbase_accession"
-    
-    #ortholog=$(echo "$k" | jq  "$property_ortholog")
-    #echo "ortholog - $ortholog"
-    
-    symbol=$(echo "$k" | jq "$property_symbol")
-    echo "symbol - $symbol"
+  ensgene=$(mutiple_values_with_separator "$k" "$property_ensgene")
+  echo "ensgene - $ensgene"
+  
+  #mirbase_accession=$(mutiple_values_with_separator "$k" "$property_mirbase_accession")
+  #echo "mirbase_accession - $mirbase_accession"
+  
+  #ortholog=$(mutiple_values_with_separator "$k" "$property_ortholog")
+  #echo "ortholog - $ortholog"
+  
+  symbol=$(mutiple_values_with_separator "$k" "$property_symbol")
+  echo "symbol - $symbol"
 
-    goterm=$(echo "$k" | jq "$property_goterm")
-    echo "goterm - $goterm"
-    
-    ensfamily=$(echo "$k" | jq "$property_ensfamily")
-    echo "ensfamily - $ensfamily"
-    
-    uniprot=$(echo "$k" | jq "$property_uniprot")
-    echo "uniprot - $uniprot"
-    
-    description=$(echo "$k" | jq "$property_description")
-    echo "description - $description"
-    
-    ensprotein=$(echo "$k" | jq "$property_ensprotein")
-    echo "ensprotein - $ensprotein"
-    
-    interpro=$(echo "$k" | jq "$property_interpro")
-    echo "interpro - $interpro"
-    
-    gene_biotype=$(echo "$k" | jq "$property_gene_biotype")
-    echo "gene_biotype - $gene_biotype"
-    
-    embl=$(echo "$k" | jq "$property_embl")
-    echo "embl - $embl"
-    
-    #mirbase_id=$(echo "$k" | jq  "$property_mirbase_id")
-    #echo "mirbase_id - $mirbase_id"
-    
-    hgnc_symbol=$(echo "$k" | jq "$property_hgnc_symbol")
-    echo "hgnc_symbol - $hgnc_symbol"
+  goterm=$(mutiple_values_with_separator "$k" "$property_goterm")
+  echo "goterm - $goterm"
+  
+  ensfamily=$(mutiple_values_with_separator "$k" "$property_ensfamily")
+  echo "ensfamily - $ensfamily"
+  
+  uniprot=$(mutiple_values_with_separator "$k" "$property_uniprot")
+  echo "uniprot - $uniprot"
+  
+  description=$(mutiple_values_with_separator "$k" "$property_description")
+  echo "description - $description"
+  
+  ensprotein=$(mutiple_values_with_separator "$k" "$property_ensprotein")
+  echo "ensprotein - $ensprotein"
+  
+  interpro=$(mutiple_values_with_separator "$k" "$property_interpro")
+  echo "interpro - $interpro"
+  
+  gene_biotype=$(mutiple_values_with_separator "$k" "$property_gene_biotype")
+  echo "gene_biotype - $gene_biotype"
+  
+  embl=$(mutiple_values_with_separator "$k" "$property_embl")
+  echo "embl - $embl"
+  
+  #mirbase_id=$(mutiple_values_with_separator "$k" "$property_mirbase_id")
+  #echo "mirbase_id - $mirbase_id"
+  
+  hgnc_symbol=$(mutiple_values_with_separator "$k" "$property_hgnc_symbol")
+  echo "hgnc_symbol - $hgnc_symbol"
  
-    ensfamily_description=$(echo "$k" | jq "$property_ensfamily_description")
-    echo "ensfamily_description - $ensfamily_description"
-       
-    enstranscript=$(echo "$k" | jq "$property_enstranscript")
-    echo "enstranscript - $enstranscript"   
-    
-    interproterm=$(echo "$k" | jq "$property_interproterm")
-    echo "interproterm - $interproterm"
-    
-    refseq=$(echo "$k" | jq "$property_refseq")
-    echo "refseq - $refseq"
-    
-    entrezgene=$(echo "$k" | jq "$property_entrezgene")
-    echo "entrezgene - $entrezgene"
+  ensfamily_description=$(mutiple_values_with_separator "$k" "$property_ensfamily_description")
+  echo "ensfamily_description - $ensfamily_description"
+     
+  enstranscript=$(mutiple_values_with_separator "$k" "$property_enstranscript")
+  echo "enstranscript - $enstranscript"   
+  
+  interproterm=$(mutiple_values_with_separator "$k" "$property_interproterm")
+  echo "interproterm - $interproterm"
+  
+  refseq=$(mutiple_values_with_separator "$k" "$property_refseq")
+  echo "refseq - $refseq"
+  
+  entrezgene=$(mutiple_values_with_separator "$k" "$property_entrezgene")
+  echo "entrezgene - $entrezgene"
 
-    go=$(echo "$k" | jq "$property_go")
-    echo "go - $go"
-    
-    synonym=$(echo "$k" | jq "$property_synonym")
-    echo "synonym - $synonym"
-    
-    echo -e "\n$ensgene\tmirbase_accession\tortholog\t$symbol\t$goterm\t$ensfamily\t$uniprot\t$description\t$ensprotein\t$interpro\t$gene_biotype\t$embl\tmirbase_id\t$hgnc_symbol\t$ensfamily_description\t$enstranscript\t$interproterm\t$refseq\t$entrezgene\t$go\t$synonym" | sed 's/"//g' >> $species.ensgene.tsv
-    echo -e "\n########################\n";
+  go=$(mutiple_values_with_separator "$k" "$property_go")
+  echo "go - $go"
+  
+  synonym=$(mutiple_values_with_separator "$k" "$property_synonym")
+  echo "synonym - $synonym"
+  
+  echo -e "\n$ensgene\tmirbase_accession\tortholog\t$symbol\t$goterm\t$ensfamily\t$uniprot\t$description\t$ensprotein\t$interpro\t$gene_biotype\t$embl\tmirbase_id\t$hgnc_symbol\t$ensfamily_description\t$enstranscript\t$interproterm\t$refseq\t$entrezgene\t$go\t$synonym" | sed 's/"//g' >> $species.ensgene.tsv
+  echo -e "\n########################\n";
 done 
+
+## remove spaces between lines
+sed -i '/^ *$/d' $species.ensgene.tsv
