ade316a34fa90260e0c8241bf6f52df8ac1ebb01
Suhaib Mohammed <suhaib@ebi.ac.uk>
2020-12-15 18:02:16 +0000

    parallel processing of extarction of annotations

--- test/run_test.sh
+++ test/run_test.sh
@@ -5,6 +5,19 @@ export PATH=$scriptDir/../bin:$scriptDir/../test:$scriptDir/../annsrcs/ensembl:$
 
 IFS="
 "
+
+[ ! -z ${ENSEMBL_JSON_PATH+x} ] || (echo "Env var ENSEMBL_JSON_PATH not defined." && exit 1)
+[ ! -z ${OUTPUT_TSV_PATH+x} ] || (echo "Env var OUTPUT_TSV_PATH not defined." && exit 1)
+[ ! -z ${LOG_PATH+x} ] || (echo "Env var LOG_PATH not defined." && exit 1)
+
+
 for species_config in $(find $scriptDir/../annsrcs/ensembl -type f); do
-   annotations_from_ensembl.sh $species_config
+	
+  species=$(basename $species_config |  sed 's/\.[^ ]*//g')
+
+  rm -rf ${LOG_PATH}/${species}_config.err ${LOG_PATH}/${species}_config.out
+  touch ${LOG_PATH}/${species}_config.out ${LOG_PATH}/${species}_config.err
+
+  bsub -M 10000 -P bigmem -e ${LOG_PATH}/${species}_config.err -o ${LOG_PATH}/${species}_config.out "annotations_from_ensembl.sh $species_config"
+
 done
--- test/run_test_merge.sh
+++ test/run_test_merge.sh
@@ -9,8 +9,15 @@ IFS="
 [ ! -z ${FIELD_NAME1+x} ] || (echo "Env var FIELD_NAME1 not defined." && exit 1)
 [ ! -z ${FIELD_NAME2+x} ] || (echo "Env var FIELD_NAME2 not defined." && exit 1)
 [ ! -z ${OUTPUT_PATH+x} ] || (echo "Env var OUTPUT_PATH not defined." && exit 1)
+[ ! -z ${LOG_PATH+x} ] || (echo "Env var LOG_PATH not defined." && exit 1)
 
 for species_file in $(find $ANNOTATION_PATH -type f); do
-  echo "extracting from $FIELD_NAME1 - $FIELD_NAME2 $species_file"
-  merge_gene_attributes.sh $species_file $FIELD_NAME1 $FIELD_NAME2 
+	
+	species=$(basename $species_file |  sed 's/\.[^ ]*//g')
+	echo "extracting from $FIELD_NAME1 - $FIELD_NAME2 $species_file"
+	rm -rf ${LOG_PATH}/${species}_merge.err ${LOG_PATH}/${species}_merge.out
+	touch ${LOG_PATH}/${species}_merge.out ${LOG_PATH}/${species}_merge.err
+
+	bsub -M 5000 -P bigmem -e ${LOG_PATH}/${species}_merge.err -o ${LOG_PATH}/${species}_merge.out "merge_gene_attributes.sh $species_file $FIELD_NAME1 $FIELD_NAME2"
+
 done
